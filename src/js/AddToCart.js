16789c74e2c08ee3fe25e6100cca8941a04370fc
qr1 <crt782@gmail.com>
2015-05-21 00:14:01 +0300

    AddToCart:  buttons and items now getting  the same id as purchases

--- src/js/AddToCart.js
+++ src/js/AddToCart.js
@@ -11,8 +11,9 @@ $("#list").delegate("button",'click',function(){
              purchases.push(purchase);
             console.log(purchases);
             $.each( purchases, function(i, purchase){
-                purchases.push("<li>" + purchase.id +
-                "<br>" + purchase.name + "<br>" + "Price:" +purchase.price + "<br>" + "<button type='button' id='purchase.id' class='btn-default'>remove</button>" +"</li>" );
+                purchases.push("<li id='"+ purchase.id +"'>" + purchase.id +
+                "<br>" + purchase.name + "<br>" + "Price:" +purchase.price + "<br><button id='"+purchase.id+"' type='button' class='btn-default'>remove</button>" +"</li>" );
+
             });
             $('#ordersUl').append(purchases);
         }
@@ -21,10 +22,6 @@ $("#list").delegate("button",'click',function(){
     //var temp = _.template($("#cartTemplate").html());
     //var getPurchase = temp({purchases: purchases});
     //$("#ordersCartDiv").html(getPurchase);
-
-
-
-
 
     //<% for (var i=0; i< purchase.length; i++) { %>
     //<li><%=purchase[i].name%><br>
@@ -36,6 +33,13 @@ $("#list").delegate("button",'click',function(){
     //<li><%= purchase[item] %></li>
     //    <% } %>
 
+});
+$("#ordersCartDiv").delegate("button","click", function() {
+    //if(this.id===$('li').attr('id')){
+    //
+    //}
+    //var id = "#" + this.id;
+    //$(id).remove();
 });
 $(window).scroll(function(){
     $("#ordersCartDiv").css({"margin-top": ($(window).scrollTop()) + "px", "margin-left":($(window).scrollLeft()) + "px"});
