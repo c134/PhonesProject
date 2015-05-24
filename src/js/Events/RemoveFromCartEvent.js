c3e29d08467286e1d5f54708f3d1a20599a7a3f3
qr1 <crt782@gmail.com>
2015-05-25 01:07:32 +0300

    Items now can be removed  from cart.

--- src/js/Events/RemoveFromCartEvent.js
+++ src/js/Events/RemoveFromCartEvent.js
@@ -2,5 +2,6 @@ $("#ordersCartDiv,#cartList").on("click","button", function () {
     var buttonId = $(this).attr('id');
     RemoveFromCart(buttonId,Cart.items);
     console.log(Cart.items);
+    Cart.CalculateTotal();
     RenderCart.render("ordersCartDiv", "cartTemplate", Cart.items);
 });
