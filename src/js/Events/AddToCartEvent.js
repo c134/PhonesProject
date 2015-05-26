51f723ad5a41a4294a8a88e84e3889fa0fcd5836
qr1 <crt782@gmail.com>
2015-05-26 16:52:18 +0300

    Removed old views, controllers,event

--- src/js/Events/AddToCartEvent.js
+++
@@ -1,7 +0,0 @@
-$("#list").on('click' ,"button",function(){
-    var buttonId = $(this).attr('id');
-    AddToCart(buttonId, phones,Cart.items);
-    console.log(Cart.items);
-    Cart.CalculateTotal();
-    RenderCart.render("ordersCartDiv", "cartTemplate", Cart.items);
-});
--- src/js/Events/RemoveFromCartEvent.js
+++
@@ -1,7 +0,0 @@
-$("#ordersCartDiv,#cartList").on("click","button", function () {
-    var buttonId = $(this).attr('id');
-    RemoveFromCart(buttonId,Cart.items);
-    console.log(Cart.items);
-    Cart.CalculateTotal();
-    RenderCart.render("ordersCartDiv", "cartTemplate", Cart.items);
-});
