2811fe5cf5f0458782000804b1c9a3e70428a15d
qr1 <crt782@gmail.com>
2015-05-26 18:26:13 +0300

    Old model was removed. Created: itemList, cartCollections, cartList.

--- src/js/Models/CartModel.js
+++ src/js/Models/CartModel.js
@@ -1,19 +1,90 @@
-var Cart={
-    items: [],
-    total: 0,
-    quantity: 0,
-    CalculateTotal: function(){
-        var _total = 0;
-        var _price = 0;
-        for(var i = 0; i < this.items.length; i++){
-            this.quantity = this.items.length;
-            _price = this.items[i].price;
-            _total += _price;
-            this.total = _total;
-        }
-        console.log(this.total);
-        console.log(this.quantity);
+var itemList = {
+    _items: null,
+    _template: null,
+    _rootElement: null, // для чего нужно это свойство так и осталось для меня загадкой.
+    initialize:function(divId, items){
+        this.divId = divId;
+        this._items = items;
+        this.render();
+        this._addEvents();
+    },
+    render:function(){
+        var itemsCopy = this._items;
+        this._template = _.template($("#listTemplate").html());
+        var rendTemplate = this._template({items: itemsCopy});
+        $("#" +this.divId).html(rendTemplate);
+    },
+    _addEvents:function(){
+        $("#list").on('click','button', function(){
+            var buttonId = $(this).attr('id');
+            cartCollection.addItem(buttonId,itemList._items);
+            cartCollection.getTotalPrice();
+            cartList.initialize("ordersCartDiv");
+        });
     }
-}
+};
+var cartCollection = {
+    _items: [],
+    _quantity: 0,
+    _total: 0,
+    addItem: function(buttonId, arr){
+        this.buttonId = buttonId;
+        this.arr = arr;
+        var result = _.findWhere(this.arr,{id:this.buttonId});
+        var purchase ={
+            id: result.id,
+            name: result.name,
+            price:result.price
+        };
+        this._items.push(purchase);
+    },
+    removeItem:function(buttonId){
+        this.buttonId = buttonId;
+        for(var i = 0; i < this._items.length; i++){
+            if(this.buttonId == this._items[i].id){
+                this._total -= this._items[i].price;
+                this._items.splice(i,1);
+                break;
+            }
+        }
+    },
+    getTotalPrice: function(){
+        console.log(this._items);
+        this._total = 0;
+        for(var i = 0; i < this._items.length; i++){
+           this._quantity = this._items.length;
+            this._total += this._items[i].price;
+        }
+    }
+};
+var cartList = {
+    _itemsCollections: cartCollection,
+    _template: null,
+    _rootElement: null,
+    initialize: function(divId){
+        this.divId = divId;
+        cartList.render();
+        this._addEvents();
+    },
+    render:function(){
+        var data={
+            items: cartCollection._items,
+            total:cartCollection._total ,
+            quantity:cartCollection._quantity
+        };
+        this._template = _.template($("#cartTemplate").html());
+        var rendTemplate = this._template({data: data});
+        $("#" + this.divId).html(rendTemplate);
+    },
+    _addEvents:function() {
+        $("#cartList").on('click','button', function () {
+            var buttonId = $(this).attr('id');
+            cartCollection.removeItem(buttonId);
+            cartCollection.getTotalPrice();
+            cartList.initialize("ordersCartDiv");
+        });
+    }
+};
+itemList.initialize("phonesDiv",phones);
 
 
