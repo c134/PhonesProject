da985979c062d168c8b6495f7042c700a95875f4
qr1 <crt782@gmail.com>
2015-05-28 22:41:58 +0300

    Minor changes in structure of cartCollection. cartList , itemList were moved to corresponding files.

--- src/js/Models/CartModel.js
+++ src/js/Models/CartModel.js
@@ -1,35 +1,7 @@
-var itemList = {
-    _items: null,
-    _template: _.template($("#listTemplate").html()),
-    _rootElement: $("#phonesDiv"),
-    initialize: function (items) {
-        this._items = items;
-        this.render();
-        this._addEvents();
-    },
-    render: function () {
-        var itemsCopy = _.map(this._items, _.clone);
-        var rendTemplate = this._template({items: itemsCopy});
-        this._rootElement.html(rendTemplate);
-    },
-    _addEvents: function () {
-        $("#list,.btn-success").on('click', function () {
-            var buttonId = $(this).attr('id');
-            var result = _.findWhere(itemList._items, {id: buttonId});
-            var purchase = {
-                id: result.id,
-                name: result.name,
-                price: result.price
-            };
-            cartCollection.addItem(purchase);
-            cartList.render();
-        });
-    }
-};
 var cartCollection = {
     _items: [],
-    addItem: function (arr) {
-        this._items.push(arr);
+    addItem: function (obj) {
+        this._items.push(obj);
     },
     removeItem: function (removeItem) {
         this._items = _.without(this._items, _.findWhere(this._items, removeItem));
@@ -46,34 +18,7 @@ var cartCollection = {
         return total;
     },
     getQuantity: function () {
-        var quantity = 0;
-        return quantity = this._items.length;
-    }
-};
-var cartList = {
-    _itemsCollections: cartCollection,
-    _template: _.template($("#cartTemplate").html()),
-    _rootElement: $("#ordersCartDiv"),
-    initialize: function (divId) {
-        this.divId = divId;
-    },
-    render: function () {
-        var data = {
-            items: this._itemsCollections.getItems(),
-            total: this._itemsCollections.getTotalPrice(),
-            quantity: this._itemsCollections.getQuantity()
-        };
-        var rendTemplate = this._template({data: data});
-        this._rootElement.html(rendTemplate);
-        this._addEvents();
-    },
-    _addEvents: function () {
-        $(this.divId + ", .btn-default").on('click', function () {
-            var buttonId = $(this).attr('id');
-            var itemToRemove = _.findIndex(cartCollection._items, buttonId);
-            cartCollection.removeItem(itemToRemove);
-            cartList.render();
-        });
+        return this._items.length;
     }
 };
 itemList.initialize(phones);
