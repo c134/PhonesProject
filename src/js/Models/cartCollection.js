73b410f884fa06d0c84a86eab48f3a4a87c97728
qr1 <crt782@gmail.com>
2015-05-29 15:00:52 +0300

    Added: 'use strict'

--- src/js/Models/cartCollection.js
+++ src/js/Models/cartCollection.js
@@ -1,15 +1,19 @@
 var cartCollection = {
     _items: [],
     addItem: function (obj) {
+        'use strict';
         this._items.push(obj);
     },
     removeItem: function (removeItem) {
+        'use strict';
         this._items = _.without(this._items, _.findWhere(this._items, removeItem));
     },
     getItems: function () {
+        'use strict';
         return this._items;
     },
     getTotalPrice: function () {
+        'use strict';
         console.log(this._items);
         var total = 0;
         for (var i = 0; i < this._items.length; i++) {
@@ -18,10 +22,11 @@ var cartCollection = {
         return total;
     },
     getQuantity: function () {
+        'use strict';
         return this._items.length;
     }
 };
 itemList.initialize(phones);
-cartList.initialize("#cartList");
+cartList.initialize('#cartList');
 
 
