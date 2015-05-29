8d934ebdd0dcef84c841278d25550e5ca047e4d2
qr1 <crt782@gmail.com>
2015-05-29 15:01:37 +0300

    Added: 'use strict'

--- src/js/Models/itemList.js
+++ src/js/Models/itemList.js
@@ -1,18 +1,21 @@
 var itemList = {
     _items: null,
-    _template: _.template($("#listTemplate").html()),
-    _rootElement: $("#phonesDiv"),
+    _template: _.template($('#listTemplate').html()),
+    _rootElement: $('#phonesDiv'),
     initialize: function (items) {
+        'use strict';
         this._items = items;
         this.render();
         this._addEvents();
     },
     render: function () {
+        'use strict';
         var itemsCopy = _.map(this._items, _.clone);
         var rendTemplate = this._template({items: itemsCopy});
         this._rootElement.html(rendTemplate);
     },
     _addEvents: function () {
+        'use strict';
         this._rootElement.on('click','.buyButton' ,function () {
             var buttonId = $(this).attr('id');
             var result = _.findWhere(itemList._items, {id: buttonId});
