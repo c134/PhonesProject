0d41bfb910e2a517690cda2835b4579285c14efa
qr1 <crt782@gmail.com>
2015-05-29 15:01:16 +0300

    Added: 'use strict'

--- src/js/Models/cartList.js
+++ src/js/Models/cartList.js
@@ -1,14 +1,16 @@
 var cartList = {
     _itemsCollections: cartCollection,
-    _template: _.template($("#cartTemplate").html()),
-    _rootElement: $("#ordersCartDiv"),
+    _template: _.template($('#cartTemplate').html()),
+    _rootElement: $('#ordersCartDiv'),
     //_rootElementUl: $("#cartList"),
     initialize: function (divId) {
+        'use strict';
         this.divId = divId;
         //this._rootElementUl = $("#cartList");
         this._addEvents();
     },
     render: function () {
+        'use strict';
         var data = {
             items: this._itemsCollections.getItems(),
             total: this._itemsCollections.getTotalPrice(),
@@ -19,6 +21,7 @@ var cartList = {
         this._addEvents();
     },
     _addEvents: function () {
+        'use strict';
         $(this.divId).on('click','.removeButton' , function () {
             var buttonId = $(this).attr('id');
             var itemToRemove = _.findIndex(cartCollection._items, buttonId);
