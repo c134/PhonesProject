33c01693109d8310f0a240cbe80d484a2da37e23
qr1 <crt782@gmail.com>
2015-05-30 19:17:52 +0300

    Now calculates price and quantity for each item in cart.

--- src/js/Models/cartCollection.js
+++ src/js/Models/cartCollection.js
@@ -2,28 +2,60 @@ var cartCollection = {
     _items: [],
     addItem: function (obj) {
         'use strict';
-        this._items.push(obj);
+        var result = _.findWhere(this._items, {id: obj.id});
+        if(result){
+            result.quantity++;
+        }
+        else{
+            this._items.push(obj);
+        }
     },
     removeItem: function (removeItem) {
         'use strict';
-        this._items = _.without(this._items, _.findWhere(this._items, removeItem));
+        console.log(removeItem);
+        var result = _.findWhere(this._items, removeItem);
+         if(result.quantity > 0){
+            result.quantity--;
+            this._items[removeItem] = result;
+        }
+        else if(result.quantity === 0){
+            this._items = _.without(this._items, _.findWhere(this._items, removeItem));
+        }
     },
     getItems: function () {
         'use strict';
         return this._items;
+    },
+    getItemTotalPrice: function(){
+        'use strict';
+        var itemTotal = 0;
+        var price = 0;
+        var quantity = 0;
+        for(var i = 0; i < this._items.length; i++){
+            price = this._items[i].price;
+            quantity = this._items[i].quantity;
+            itemTotal = price * quantity;
+            this._items[i].thisItemTotal = itemTotal;
+            console.log(itemTotal);
+        }
+        return itemTotal;
     },
     getTotalPrice: function () {
         'use strict';
         console.log(this._items);
         var total = 0;
         for (var i = 0; i < this._items.length; i++) {
-            total += this._items[i].price;
+            total += this._items[i].thisItemTotal;
         }
         return total;
     },
-    getQuantity: function () {
+    getTotalQuantity: function () {
         'use strict';
-        return this._items.length;
+        var totalQnt = 0;
+         for(var i = 0; i < this._items.length; i++){
+             totalQnt += this._items[i].quantity;
+         }
+        return totalQnt;
     }
 };
 itemList.initialize(phones);
